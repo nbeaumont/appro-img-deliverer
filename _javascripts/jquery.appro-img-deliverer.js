25015e002a8cf4492da256620292adc86e888be9
nbeaumont <n.beaumont617@gmail.com>
2015-11-26 22:35:33 -0500

    Update jquery.appro-img-deliverer.js

--- _javascripts/jquery.appro-img-deliverer.js
+++ _javascripts/jquery.appro-img-deliverer.js
@@ -90,8 +90,7 @@
     };
     $(document).ready(function() {
         $(document.body).prepend("<div id='aid-mediaQueryIndicator'></div>");
-        $('.aid-main').each(function(event) {
-            event.stopPropagation()
+        $('.aid-main').each(function() {
             $(this).prepend("<i class='fa fa-search-plus fa-lg aid-zoomIcon'></i>").wrap("<div class='aid-contentWrapper'></div>").wrap("<div class='aid-centerer'></div>").wrap("<div class='aid-wrapper'></div>");
             $(this).aid_set_img();
             $(this).aid_open_popup();
